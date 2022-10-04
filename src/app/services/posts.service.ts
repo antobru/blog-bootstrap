9257e9b08fc029f539d8c1869f093e673953d1a3
addoeventi <antonio@oddastudio.com>
2022-10-04 15:32:46 +0200

    Add support for server

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Post } from '../models/post.model';
 import { Comment } from '../models/comment.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +12,23 @@ export class PostsService {
   constructor(private http: HttpClient) { }
 
   findAll() {
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').toPromise();
+    return this.http.get<Post[]>(environment.BASE_API + '/posts').toPromise();
   }
 
   findPostById(id: number) {
-    return this.http.get<Post>(`https://jsonplaceholder.typicode.com/posts/${id}`).toPromise();
+    return this.http.get<Post>(`${environment.BASE_API}/posts/${id}`).toPromise();
   }
 
   findAllCommentsByPostId(postId: number) {
-    return this.http.get<Comment[]>(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`).toPromise();
+    return this.http.get<Comment[]>(`${environment.BASE_API}/posts/${postId}/comments`).toPromise();
   }
 
   addPost(post: Post) {
-    return this.http.post<Post[]>(`https://jsonplaceholder.typicode.com/posts`, post).toPromise();
+    return this.http.post<Post[]>(`${environment.BASE_API}/posts`, post).toPromise();
   }
 
   updatePost(id: number, post: Post) {
-    return this.http.put<Post[]>(`https://jsonplaceholder.typicode.com/posts/${id}`, post).toPromise();
+    return this.http.put<Post[]>(`${environment.BASE_API}/posts/${id}`, post).toPromise();
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -10,6 +11,6 @@ export class UsersService {
   constructor(private http: HttpClient) { }
 
   findAll() {
-    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users').toPromise();
+    return this.http.get<User[]>(environment.BASE_API + '/users').toPromise();
   }
 }
